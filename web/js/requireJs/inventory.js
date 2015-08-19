c396fea2bc895e64874df59ce4ad2272a62bfcd2
ilya - mac mini <ilya-brower@yandex.ru>
2015-08-19 16:45:22 +0300

    при наведении на элемент в инвентаре, подсвечивается место куда его можно одеть

--- web/js/requireJs/inventory.js
+++ web/js/requireJs/inventory.js
@@ -1,7 +1,7 @@
 define(['jquery'], function ($) {
 
 
-var Inventory = (function() {
+    var Inventory = (function() {
 
     var $room = $('#game-room');
     var $inventory = $('#game-inventory');
@@ -23,6 +23,25 @@ var Inventory = (function() {
     }
 
         /**
+        * Основные методы
+        * @type {{}}
+        */
+        var action = {
+            /**
+             * Получение элемента инвентаря по id
+             * @param id
+             * @returns {*}
+             */
+            getItemById: function(id) {
+                var item = window.inventory.filter(function(i) {
+                    return i.itemId==id;
+                });
+
+                return item[0];
+            }
+        }
+
+        /**
          * Отображение всего, что касается инвентаря
          * @type {{allItems: allItems}}
          */
@@ -32,22 +51,38 @@ var Inventory = (function() {
 
                 var html = '';
                 window.inventory.forEach(function (item) {
-                    html += '<div class="item"><img src="/img/items/' + item.pic + '.png"></div>';
+                    html += '<div class="item" data-id="'+item.itemId+'"><img src="/img/items/' + item.pic + '.png"></div>';
                 });
 
-                $inventory.children('.items-list').html(html);
+                $inventory.children('.items-list').html(html)
+                    .on('mouseover','.item',function() {
+                        var item = action.getItemById($(this).attr('data-id'));
+
+                        /**
+                         * выделение слотов, куда можно одеть предмет
+                         */
+                        item.allowedSlots.forEach(function(as) {
+                            $('.'+as).css({'box-shadow':'0 0 4px 2px rgba(255,255,255,0.8)'});
+                        });
+
+                    })
+                    .on('mouseout','.item',function() {
+                         $('.slot').css({'box-shadow':'0 0 0 0'});
+                    });
+
+                console.log(window.inventory);
             }
 
         };
 
         return {
             init: function () {
-                $('.open-inventory-button').click(function () {
+                $('.open-inventory-button').on('click', function () {
                     openInventory();
                 });
 
 
-                $inventory.find('.close-button').click(function () { // кажется children работает быстрее fined, но не увере, надо почитать
+                $inventory.find('.close-button').on('click', function () { // кажется children работает быстрее fined, но не увере, надо почитать
                     $room.show();
                     $userInfo.hide();
                     $inventory.hide();
@@ -60,7 +95,7 @@ var Inventory = (function() {
             }
 
         }
-})();
+    })();
 
 return Inventory;
 });
